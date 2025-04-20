fa57564ec107f90765a37dbc16cd6414000b9b28
Burak Cabas <burakcabas3@gmail.com>
2025-04-20 17:30:15 +0300

    commit

--- src/main/java/burak/Practice.java
+++ src/main/java/burak/Practice.java
@@ -1,10 +1,26 @@
 package burak;
 
+import java.io.*;
+
 public class Practice {
+
+    public void scream(){
+        //public int age;
+        //using public is not allowed, cant use access modifiers inside methods
+    }
 
     public static void main(String[] args){
 
+        int _age;
+        int $age;
+        //int .age;
+        //name above won't work because we can only use $ and _ symbols
+        //variable names can have letters and numbers in them, but we can't start with a number
+        //int 6th;
+        int age_$;
+        int big_age;
 
+        String name = "Burak", surname = "Cabs";
 
     }
 
